1130d5bf9cbf3cd4de7326b8dcd3984d409b357e
Alejandro <alejandro@toro-intl.com>
2018-09-16 19:55:26 +0200

    SErvice created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-
+import { SearchService } from './services/search.service';
 @Component({
   selector: 'and-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private searchSrv: SearchService) {
+
+  }
   search(query) {
+    this.searchSrv.getVenues(query).subscribe(venues => {
+      console.log(venues);
+    });
   }
 }
